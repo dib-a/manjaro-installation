addeb6eff5392a83d9c2b7a5cb6ca167da352980
dib-a <kirakirito62@gmail.com>
2021-12-22 02:42:50 +0000

    added cryptsetup

--- installation-part-1.sh
+++ installation-part-1.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-su
-sudo pacman-mirrors --geoip && sudo pacman -Syyu
+#keep in mind to run su to become sudo before starting the script 
+
+pacman-mirrors --geoip && sudo pacman -Syyu
 systemctl enable --now systemd-timesyncd
 cfdisk /dev/sda
 mkfs.vfat -F 32 -n EFI /dev/sda1
 mkfs.ext4 -L BOOT /dev/sda2
+cryptsetup -v -y --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 2000 --use-urandom --verify-passphrase luksFormat /dev/sda3
+cryptsetup open /dev/sda3 SYSTEM
