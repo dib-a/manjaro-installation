d3be782ded57cc1d785abff2af5623c359f5268d
dib-a <kirakirito62@gmail.com>
2022-01-06 02:54:30 +0000

    script do more by itself

--- config-system.sh
+++ config-system.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-vim /etc/locale.gen
+#vim /etc/locale.gen
+cp src/locale.gen /etc/locale.gen
 locale-gen
 
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
@@ -8,17 +9,20 @@ hwclock --systohc --utc
 
 echo pc > /etc/hostname
 passwd
-vim /etc/sudoers
+#vim /etc/sudoers
+cp /src/sudoers /etc/sudoers
 
 systemctl enable dhcpcd
 systemctl enable NetworkManager
 systemctl enable systemd-timesyncd
 
-vim /etc/mkinitcpio.conf
+#vim /etc/mkinitcpio.conf
+cp src/mkinitcpio.conf /etc/mkinitcpio.conf
 mkinitcpio -P
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=manjaro --recheck
-vim /etc/default/grub
+#vim /etc/default/grub
+cp /src/grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 exit
